23fecefa265be546d72e1c3964a5c02b6437e4a7
한종호 <jongho.han@samsung.com>
2022-02-23 21:23:56 +0900

    9장 배포 자동화

---
+++ scripts/deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=sprintboot2-aws
+
+#cd $REPOSITORY/$PROJECT_NAME/
+#
+#echo "> Git Pull"
+#git pull
+#
+#echo "> 프로젝트 build 시작"
+#./gradlew build
+#
+#echo "> step1 으로 이동"
+#cd $REPOSITORY
+
+echo "> build 파일복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현개 구동중인 앱 pid 확인"
+CURRENT_PID=$(pgrep -fl sprintboot2-aws|grep jar|awk '{print $1}')
+
+echo "현재 pid : $CURRENT_PID"
+if [ -z "$CURRENT_PID" ]; then
+        echo "> 현재 구동중인 앱이 없으므로 종료하지 않습니다."
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> 새 어들리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar|tail -n 1)
+
+echo "> JAR NAME : $JAR_NAME"
+
+echo ">  $JAR_NAME 에 실행 권한 추가"
+chmod +x $JAR_NAME
+
+echo ">  $JAR_NAME 실행"
+nohup java -jar \
+        -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.profiles.active=real \
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
